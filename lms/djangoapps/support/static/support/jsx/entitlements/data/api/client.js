8427e6860660904262e69c1be1b01e6e38c1c547
Michael LoTurco <mikeloturco13@gmail.com>
2018-03-07 16:52:29 -0500

    Quality fixes

--- lms/djangoapps/support/static/support/jsx/entitlements/data/api/client.js
+++ lms/djangoapps/support/static/support/jsx/entitlements/data/api/client.js
@@ -7,57 +7,54 @@ const HEADERS = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
   'X-CSRFToken': Cookies.get('csrftoken'),
-}
+};
 
-const requestEntitlements = ({username}) => {
-  return fetch(
-    `${entitlementApi}/?user=${username}`, {
-      credentials: 'same-origin',
-      method: 'get'
-    },
-  );
-}
+const requestEntitlements = ({ username }) => fetch(
+  `${entitlementApi}/?user=${username}`, {
+    credentials: 'same-origin',
+    method: 'get',
+  },
+);
 
-const createEntitlement = ({username, courseUuid, mode, action, comments}) => {
-  return fetch(
-    `${entitlementApi}`, {
-      credentials: 'same-origin',
-      method: 'post',
-      headers: HEADERS,
-      body: JSON.stringify({
-        course_uuid: courseUuid,
-        user: username,
-        mode: mode,
-        support_details:[{
-          action: action,
-          comments: comments
-        }]
-      })
-    }
-  );
-}
 
-const updateEntitlement = ({entitlementUuid, unenrolledRun, action, comments}) => {
-  return fetch(
-    `${entitlementApi}/${entitlementUuid}`, {
-      credentials: 'same-origin',
-      method: 'patch',
-      headers: HEADERS,
-      body: JSON.stringify({
-        expired_at: null,
-        enrollment_run: null,
-        support_details:[{
-          unenrolled_run: unenrolledRun,
-          action: action,
-          comments: comments
-        }]
-      }),
-    }
-  );
-}
+const createEntitlement = ({ username, courseUuid, mode, action, comments }) => fetch(
+  `${entitlementApi}`, {
+    credentials: 'same-origin',
+    method: 'post',
+    headers: HEADERS,
+    body: JSON.stringify({
+      course_uuid: courseUuid,
+      user: username,
+      mode,
+      support_details: [{
+        action,
+        comments,
+      }],
+    }),
+  },
+);
+
+
+const updateEntitlement = ({ entitlementUuid, unenrolledRun, action, comments }) => fetch(
+  `${entitlementApi}/${entitlementUuid}`, {
+    credentials: 'same-origin',
+    method: 'patch',
+    headers: HEADERS,
+    body: JSON.stringify({
+      expired_at: null,
+      enrollment_run: null,
+      support_details: [{
+        unenrolled_run: unenrolledRun,
+        action,
+        comments,
+      }],
+    }),
+  },
+);
+
 
 export {
   requestEntitlements,
   createEntitlement,
-  updateEntitlement
+  updateEntitlement,
 };
